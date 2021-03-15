1f725f174aac245436a4baa2af6a2bdc1ebb3a3c
AndyGron <14lma.hronski.a@pdu.by>
2021-03-15 17:53:16 +0300

    feat: add finction

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let array = String(n).split('');
+    for (i = 0; i < array.length; i++) {
+        if (array[i] == '-') {
+            delete array[i];
+        }
+    }
+    return array.reverse().join('');
 }
